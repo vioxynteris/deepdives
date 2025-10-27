1582478366a87693ab32249670ac63537089d102
Vio Nguyen <vioxynteris@gmail.com>
2025-10-27 07:53:49 -0700

    Update search_seeds.rs
    Updated comments for clarity regarding seed range and mission stages.

--- drg_mission_gen_core/examples/search_seeds.rs
+++ drg_mission_gen_core/examples/search_seeds.rs
@@ -2,21 +2,21 @@ use drg_mission_gen_core::{gen_deep_dive_pair, EBiome, EMissionDNA, EObjective};
 
 fn main() {
     let mut count = 0;
-    for seed in 2048..0x20000 {                                                            // number values here will let you narrow the search's seed range (game supports 2048 through 131072)
-        let (normal, _hard) = gen_deep_dive_pair(seed);                                    // _hard indicated Elite dive, normal indicates regular dive
+    for seed in 2048..0x20000 {                                                            // number or hexadecimal values here will let you narrow the search's seed range (game supports 2048 through 131072)
+        let (normal, _hard) = gen_deep_dive_pair(seed);                                    // _hard indicates Elite dive, normal indicates regular dive
 
-        if _hard.missions[0].biome != EBiome::BIOME_RadioactiveZone {                      // [0] indicates Stage 1, EBiome filters biome
+        if _hard.missions[0].biome != EBiome::BIOME_RadioactiveZone {                      // [0] indicates Stage 1 (use 1 or 2 for stages 2 and 3 respectively), EBiome filters biome
             continue;
         }
         if _hard.missions[0].primary_objective != EObjective::OBJ_1st_Escort {             // EObjective can be used to fetch primary or secondary objective (in this case, Escort primary)
             continue;                                                                      // see src\data.rs for full list of objective names
         }
-         if _hard.missions[1].primary_objective != EObjective::OBJ_1st_Extraction {
+        if _hard.missions[1].primary_objective != EObjective::OBJ_1st_Extraction {
             continue;
         }
-         if _hard.missions[1].dna != EMissionDNA::DNA_2_01 {                               // EMissionDNA allows you to specify objective by length/complexity,
+        if _hard.missions[1].dna != EMissionDNA::DNA_2_01 {                                // EMissionDNA allows you to specify objective by length/complexity,
             continue;                                                                      // and does not need to be preceded by an EObjective search for its stage
-         }
+        }
         if _hard.missions[2].primary_objective != EObjective::OBJ_1st_Facility {
             continue;
         }
